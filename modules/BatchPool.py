40aca31c55c6218b1244f51ba9ddd5f72fb5c5fc
Dhruv Narayanan <thedhruvn@gmail.com>
2020-09-20 16:55:33 -0500

    Tweaked copy and apt-handling commands

--- modules/BatchPool.py
+++ modules/BatchPool.py
@@ -228,12 +228,12 @@ class BatchPool:
                 'usermod -aG sudo azureuser',
                 'cd /home/polycraft',
                 'chmod -R 777 *',
-                'rm /home/polycraft/oxygen/mods/polycraft-1.5.2-20200909-21.14.01.jar',
+                'rm /home/polycraft/oxygen/mods/*.jar',
                 # Stop the crontabs from running
                 'sudo rm /var/spool/cron/crontabs/*',
                 # 'sudo touch /var/spool/cron/crontabs/polycraft && sudo chmod 0 /var/spool/cron/crontabs/polycraft',
                 'while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do sleep 1; done; sudo apt-get install software-properties-common -y',
-                'sudo apt-add-repository universe',
+                'while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do sleep 1; done; sudo apt-add-repository universe',
                 # Mount the Polycraft Game FileShare
                 'while fuser /var/lib/dpkg/lock >/dev/null 2>&1; do sleep 1; done; sudo apt-get install cifs-utils -y && sudo mkdir -p /mnt/PolycraftGame/',
                 f'mount -t cifs //polycraftbestbatch.file.core.windows.net/best-batch-round-1-test /mnt/PolycraftGame -o vers=3.0,username={self.credentials.get("Storage", "storageaccountname")},password={self.credentials.get("Storage", "storageaccountkey")},dir_mode=0777,file_mode=0777,serverino && ls /mnt/PolycraftGame',
