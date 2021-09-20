4ab5c2b0a9f9486cfc76fe3bedb0d7c8fa7dee44
Dhruv <thedhruvn@users.noreply.github.com>
2021-09-19 22:46:00 -0500

    Tweaked the order of checks

--- scripts/update_git_and_restart_polycraft.sh
+++ scripts/update_git_and_restart_polycraft.sh
@@ -6,38 +6,45 @@
 
 pull_and_copy() {
   # Check if $1 is a git repository
-  if [[ -d $1 ]]; then
-    cd "$1" || return 1;
-    echo "Checking if $PWD is a git directory";
-    git status --porcelain || return 1; # A fatal error if $PWD is not a git repo
-    if git pull | grep -q "Already up to date"; then
-      echo "repo is up to date";
-      return 1
-    fi
-    if [[ -d $1/scripts/ ]] && [[ -d $2/mods/ ]]; then
-      rm -f $2/mods/*.jar
-      cp $1/scripts/*.jar $2/mods/
-      return 0
-    else
-      echo "Error! scripts doesn't exist or $2 is not valid";
-
-    fi
+#  if [[ -d $1 ]]; then
+#    cd "$1" || return 1;
+#    echo "Checking if $PWD is a git directory";
+#    git status --porcelain || return 1; # A fatal error if $PWD is not a git repo
+  if git pull | grep -q "Already up to date"; then
+    echo "repo is up to date";
+    return 1
+  fi
+  if [[ -d $1/scripts/ ]] && [[ -d $2/mods/ ]]; then
+    rm -f $2/mods/*.jar
+    cp $1/scripts/*.jar $2/mods/
+    return 0
   else
-    echo "Error: input is not a directory!";
+    echo "Error! $1/scripts doesn't exist or $2/mods/ is not valid";
   fi
   return 1;
 
 }
+
 temp1=1;
-if [ "$(git rev-parse HEAD)" = "$(git ls-remote "$(git rev-parse --abbrev-ref @{u} | sed 's/\// /g')" | cut -f1)" ]; then
-  echo up to date;
+if [[ -d "$1" ]]; then
+    cd "$1" || return 1;
+    echo "Checking if $PWD is a git directory";
+    git status --porcelain || return 1; # A fatal error if $PWD is not a git repo
+
+  if [ "$(git rev-parse HEAD)" = "$(git ls-remote "$(git rev-parse --abbrev-ref @{u} | sed 's/\// /g')" | cut -f1)" ]; then
+    echo up to date;
+  else
+    pull_and_copy "$1" "$2";
+    temp1=$?;
+  fi
+  # Check if the previous result was 0 (successful pull) or not
+  # Confirm that there is a 3rd input argument to run polycraft no pp.
+  if [[ temp1 -eq 0 ]] && [[ $# -gt 2 ]]; then
+    bash run_polycraft_no_pp.sh "$3"
+  fi
+
 else
-  temp1=pull_and_copy "$1" "$2";
+    echo "Error: input is not a directory!";
 fi
 
 
-# Check if the previous result was 0 (successful pull) or not
-# Confirm that there is a 3rd input argument to run polycraft no pp.
-if [[ temp1 -eq 0 ]] && [[ $# -gt 2 ]]; then
-  bash run_polycraft_no_pp.sh $3
-fi
